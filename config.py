424bd080564548ba701ac33aa05e9693dc6ce877
nanana007 <lkyuier@hotmail.com>
2024-09-01 14:42:08 +0800

    Initial commit

---
+++ config.py
@@ -0,0 +1,28 @@
+# config.py
+
+# Configuration Constants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+MAX_TEXT_WIDTH = 100
+PERSON_SPEED = 5
+JUMP_SPEED = -15
+GRAVITY = 0.5
+NUM_SHARDS = 30
+NUM_MONSTERS = 5
+FONT_PATH = "C:/Windows/Fonts/simhei.ttf"
+FONT_SIZE = 20
+TILE_SIZE = 50
+
+# Dialogs Constants
+DIALOGS_ALL = [
+    "bailiu: 在必要的时候你伸手帮我一下，我们互利互惠，互相合作怎么样？",
+    "mushicheng:OK,那就合作吧。",
+    "mushicheng:我们信息共享，互相帮助，我不白用，1积分的诚意给你。",
+    "mushicheng:我的诚意就是--",
+    "mushicheng:白柳你哭的诚恳一点，我也不是不能勉为其难地伸出援手。",
+    "god: 恭喜玩家白柳首先触发主线任务--",
+    "god: 收集末班车上的碎镜片",
+    "mushicheng: 白柳，你怎么触发的主线任务？你都没动过。",
+    "bailiu: 我怎么没动。我脑子在动啊，牧四诚。",
+    "bailiu: "
+]
